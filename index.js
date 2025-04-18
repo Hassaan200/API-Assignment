7cc68303d5e4db281ef2f3a4eef3b35b3e6e2b96
Hassaan200 <hassaankhaliq45@gmail.com>
2025-04-18 22:09:30 +0500

    integrate serach field

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 fetch('https://dummyjson.com/products')
-.then(response => response.json()) 
-.then(data => {
-    const products = data.products; 
-    const productGrid = document.getElementById("productGrid");
+    .then(response => response.json())
+    .then(data => {
+        const products = data.products; 
+        const productGrid = document.getElementById("productGrid");
+        const searchInput = document.getElementById("searchInput");
+        productGrid.innerHTML = "";
 
-    
-    productGrid.innerHTML = "";
+        // Function to display products
+        function displayProducts(productsToDisplay) {
+            productGrid.innerHTML = "";
+            productsToDisplay.forEach(product => {
+                const productCard = document.createElement("div");
+                productCard.classList.add("product-card");
+                productCard.innerHTML = `
+                    <img src="${product.thumbnail}" alt="${product.title}">
+                    <h3>${product.title}</h3>
+                    <p>Price: Rs ${product.price}</p>
+                    <p>${product.description.substring(0, 50)}...</p>
+                    <button>Add to card</button>
+                `;
+                productGrid.appendChild(productCard);
+            });
+        }
 
-    
-    products.forEach(product => {
-        const productCard = document.createElement("div");
-        productCard.classList.add("product-card");
-        productCard.innerHTML = `
-            <img src="${product.thumbnail}" alt="${product.title}">
-            <h3>${product.title}</h3>
-            <p>Price: Rs ${product.price}</p>
-            <p>${product.description.substring(0, 50)}...</p>
-            <button>Add to card</button>
-        `;
-        productGrid.appendChild(productCard);
-        console.log(product.description)
-    });
-})
+        // show product into browser 
+        displayProducts(products);
+
+        // Real-time search happen
+        searchInput.addEventListener('input', () => {
+            const searchTerm = searchInput.value.toLowerCase();
+                const filteredProducts = products.filter(product => 
+                    
+                      product.title.toLowerCase().includes(searchTerm) || 
+                        product.description.toLowerCase().includes(searchTerm)
+                    
+                
+                
+                )
+                
+            displayProducts(filteredProducts);
+        });
+
+    })
+
 .catch(error => {
     console.log("Error fetching products:", error);
     document.getElementById("productGrid").innerHTML = "<p class = 'fail'>Failed to load products.</p>";
